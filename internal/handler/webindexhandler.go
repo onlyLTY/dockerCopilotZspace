cd9bd14afed0ec228b88b0e8a1569cdc1c368e13
onlyLTY <46625336+onlyLTY@users.noreply.github.com>
2024-02-05 20:41:43 +0800

    fix: 修复访问空白的问题

--- internal/handler/webindexhandler.go
+++ internal/handler/webindexhandler.go
@@ -3,19 +3,12 @@ package handler
 import (
 	"net/http"
 
-	"github.com/onlyLTY/dockerCopilotZspace/zspace/internal/logic"
 	"github.com/onlyLTY/dockerCopilotZspace/zspace/internal/svc"
-	"github.com/zeromicro/go-zero/rest/httpx"
 )
 
 func webindexHandler(svcCtx *svc.ServiceContext) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		l := logic.NewWebindexLogic(r.Context(), svcCtx)
-		err := l.Webindex()
-		if err != nil {
-			httpx.ErrorCtx(r.Context(), w, err)
-		} else {
-			httpx.Ok(w)
-		}
+		w.Header().Set("Location", "/manager")
+		w.WriteHeader(301)
 	}
 }
