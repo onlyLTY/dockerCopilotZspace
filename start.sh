6dc0440219e0cf2340abf54020c3a0b6ff5fcc61
onlyLTY <46625336+onlyLTY@users.noreply.github.com>
2024-02-05 18:24:32 +0800

    fix: 修复启动脚本错误

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
-# 判断当前目录下是否存在名为 dockerCopilotZspace-new 的二进制文件
-if [ -f "./dockerCopilotZspace-new" ]; then
-    # 如果存在，则用它覆盖 dockerCopilotZspace
-    mv ./dockerCopilotZspace-new ./dockerCopilotZspace
+# 判断当前目录下是否存在名为 dockerCopilot-new 的二进制文件
+if [ -f "./dockerCopilot-new" ]; then
+    # 如果存在，则用它覆盖 dockerCopilot
+    mv ./dockerCopilot-new ./dockerCopilot
     # 赋予 dockerCopilotZspace 执行权限
-    chmod +x ./dockerCopilotZspace
+    chmod +x ./dockerCopilot
 fi
 
-# 运行 dockerCopilotZspace
-./dockerCopilotZspace
+# 运行 dockerCopilot
+./dockerCopilot
